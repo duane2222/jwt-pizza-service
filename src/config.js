f2872ba5d525b98f607e4789f0633f2bdc7b810b
duane2 <duane2@byu.edu>
2024-10-17 17:54:57 -0600

    added in Dockerfile

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ module.exports = {
   jwtSecret: 'b90294hijkfd2fdsaf',
   db: {
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'tempdbpassword',
+        host: 'host.docker.internal',
+        user: 'duane197',
+        password: 'Qwertymoon1',
       database: 'pizza',
       connectTimeout: 60000,
     },
